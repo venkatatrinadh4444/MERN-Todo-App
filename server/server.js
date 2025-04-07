76bcfda71050cbec705394ebdebd4a7a88848485
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-04-07 18:11:23 +0530

    Updated origin with Frontend url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT=process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log("MONGO DB connected successfully!")).catch(err=>console.log("Error occured at MONGO DB connection",err))
 
-app.use(cors({origin:"*"}))
+app.use(cors({origin:"https://mern-todo-app-sage.vercel.app/",credentials:true}))
 app.use(express.json())
 app.use('/todos',routes)
 
